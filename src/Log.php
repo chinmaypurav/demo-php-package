b99df3b1bb8a15fa70e7246aa833b28d958674fe
Chinmay Purav <70144052+chinmaypurav@users.noreply.github.com>
2021-09-24 17:44:26 +0530

    first commit

---
+++ src/Log.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Chinmay\DemoLogger;
+
+class Log
+{
+    public static function info(string $string, array $context)
+    {
+        return 'You got the wrong namespace';
+    }
+}
